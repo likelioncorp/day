568bbc7a4901b495442237f5248bba71876d6430
임광국 <kwangkuk0821@gmail.com>
2015-03-05 12:54:58 +0900

    countdown 함수
    ToyId: 96f834ff1d112af1f8e2f02465e6b1a25acab064

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,4 +2,20 @@ window.onload = function() {
   var days, hours, minutes, seconds;
   var target_date = new Date("11,12,2015").getTime();
   var d_day = document.getElementById("d-day");
+
+  function countdown() {
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;  
+
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+     
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+
+    return days + "일 " + hours + "시간 " + minutes + "분 " + seconds + "초";
+  }
 }
